c4d53082d215b8e44492b0ece4105d0a2090772a
19214Romanov <55284841+19214Romanov@users.noreply.github.com>
2019-10-22 17:28:04 +0700

    Update hs1.hs

--- hs1.hs
+++ hs1.hs
@@ -1,8 +1,8 @@
-quadrat a b c | (descr a b c < 0) = error "Can't find real roots"
+quadrat a b c | (descr < 0) = error "Can't find real roots"
               | (a == 0) && (b == 0) = error "Infinity"
               | (a == 0) && (b /= 0) = [-c / b]
-              | ((descr a b c >= 0) && (a /= 0)) = [rootOne a b c, rootTwo a b c]
+              | ((descr >= 0) && (a /= 0)) = [rootOne, rootTwo]
               | otherwise = error "It's not quadratic equation"
-              where descr a b c = b * b - 4 * a * c
-                    rootOne a b c = ((-b + sqrt(b * b - 4 * a * c)) / (2 * a))
-                    rootTwo a b c = ((-b - sqrt(b * b - 4 * a * c)) / (2 * a))
+              where descr = b * b - 4 * a * c
+                    rootOne = ((-b + sqrt(b * b - 4 * a * c)) / (2 * a))
+                    rootTwo = ((-b - sqrt(b * b - 4 * a * c)) / (2 * a))
