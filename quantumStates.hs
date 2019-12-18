4dde62843c9f0a9333d680ce69faa7deb4cae77f
19214Romanov <55284841+19214Romanov@users.noreply.github.com>
2019-12-18 15:34:24 +0700

    Update quantumStates.hs

--- quantumStates.hs
+++ quantumStates.hs
@@ -1,9 +1,9 @@
 type Reeall = Integer
 type Imagine = Integer
-data ComplexNumber = ComplexNumber Reeall Imagine
+data ComplexNumber a = ComplexNumber a a
 
-instance Show (ComplexNumber) where
-     show(ComplexNumber 0 0) = "0"
+instance (Num a, Show a, Ord a, Eq a, Integral a) => Show (ComplexNumber a) where
+     show(ComplexNumber 0 0) = show 0
      show(ComplexNumber 0 1) = "i"
      show(ComplexNumber 0 (-1)) = "-i"
      show(ComplexNumber a 0) = show a
@@ -14,17 +14,17 @@ instance Show (ComplexNumber) where
                              | otherwise = show a ++ " - " ++ show (abs b) ++ "i"
 
 
-instance Eq (ComplexNumber) where
+instance (Num a, Show a, Ord a, Eq a, Integral a) => Eq (ComplexNumber a) where
      (ComplexNumber a1 b1) == (ComplexNumber a2 b2) = z1 == z2 where
          z1 = sqrt (fromIntegral(a1^2) + fromIntegral(b1^2))
          z2 = sqrt (fromIntegral(a2^2) + fromIntegral(b2^2))
 
-instance Ord (ComplexNumber) where
+instance (Num a, Show a, Ord a, Eq a, Integral a) => Ord (ComplexNumber a) where
      (ComplexNumber a1 b1) `compare` (ComplexNumber a2 b2) = z1 `compare` z2 where
          z1 = sqrt (fromIntegral(a1^2) + fromIntegral(b1^2))
          z2 = sqrt (fromIntegral(a2^2) + fromIntegral(b2^2))
 
-instance Num (ComplexNumber) where
+instance (Num a, Show a, Ord a, Eq a, Integral a) => Num (ComplexNumber a) where
      negate (ComplexNumber a b) = ComplexNumber (-a)(-b)
      (+) (ComplexNumber a1 b1) (ComplexNumber a2 b2) = (ComplexNumber (a1+a2) (b1+b2))
      (*) (ComplexNumber a1 b1) (ComplexNumber a2 b2) = (ComplexNumber (a1*a2-b1*b2) (a1*b2+b1*a2))
@@ -32,38 +32,38 @@ instance Num (ComplexNumber) where
      signum (ComplexNumber a b) | a > 0 = 1
                                 | a == 0 = 0
                                 | otherwise = -1
-     fromInteger int = ComplexNumber int 0
+     fromInteger int = ComplexNumber 0 0
 
 type StrMarker = String
-data QuantumState = ToQuantumState ComplexNumber StrMarker
-type Qubit = [QuantumState]
+data QuantumState a = ToQuantumState a StrMarker
+type Qubit a = [QuantumState a]
 
-instance Show (QuantumState) where
+instance  (Show a, Ord a, Eq a) => Show (QuantumState a) where
      show (ToQuantumState a b) = "QuantumState " ++ show a ++ " " ++ show b
 
 --не знаю как заставить это работать, но fmap работает и без этого
---instance Functor (QuantumState) where
---     fmap f (ToQuantumState complex label) = ToQuantumState (f complex) label
+instance Functor (QuantumState) where
+     fmap f (ToQuantumState complex label) = ToQuantumState (f complex) label
 
-toList :: Qubit -> [ComplexNumber]
+toList :: Qubit (ComplexNumber a) -> [ComplexNumber a]
 toList x = [complex | (ToQuantumState complex _) <- x]
 
-toLabelList :: Qubit -> [StrMarker]
+toLabelList :: Qubit (ComplexNumber a) -> [StrMarker]
 toLabelList x = [marker | (ToQuantumState _ marker) <- x]
 
-fromList :: [ComplexNumber] -> [StrMarker] -> Qubit
+fromList :: [ComplexNumber a] -> [StrMarker] -> Qubit (ComplexNumber a)
 fromList x y = [(ToQuantumState complex txt) | complex <- x, txt <- y]
 
-toPairList :: Qubit -> [(ComplexNumber, StrMarker)]
+toPairList :: Qubit (ComplexNumber a) -> [(ComplexNumber a, StrMarker)]
 toPairList x = [(complex, marker) | (ToQuantumState complex marker) <- x]
 
-fromPairList :: [(ComplexNumber, StrMarker)] -> Qubit
+fromPairList :: [(ComplexNumber a, StrMarker)] -> Qubit (ComplexNumber a)
 fromPairList x = [(ToQuantumState complex marker) | (complex, marker) <- x]
 
-scalarProduct :: Qubit -> Qubit -> ComplexNumber
+scalarProduct :: (Num a, Show a, Ord a, Eq a, Integral a) => Qubit (ComplexNumber a) -> Qubit (ComplexNumber a) -> ComplexNumber a
 scalarProduct x y = foldr (\a b -> a + b) 0 [(complex1 * complex2)| (ToQuantumState complex1 _) <- x, (ToQuantumState complex2 _) <- y]
 
-entagle :: Qubit -> Qubit -> Qubit
+entagle :: (Num a, Show a, Ord a, Eq a, Integral a) => Qubit (ComplexNumber a) -> Qubit (ComplexNumber a) -> Qubit (ComplexNumber a)
 entagle x y = [(ToQuantumState (complex1 * complex2) (marker1 ++ marker2)) | (ToQuantumState complex1 marker1) <- x, (ToQuantumState complex2 marker2) <- y]
 
 --Examples
