8ec4777ef8933bc0f243d7de4f9c370aa493967c
19214Romanov <55284841+19214Romanov@users.noreply.github.com>
2020-04-29 17:14:23 +0700

    Update PROJ1.hs

--- PROJ1.hs
+++ PROJ1.hs
@@ -3,55 +3,57 @@ import qualified Data.ByteString.Lazy.UTF8 as BLU
 import Control.Concurrent
 import Control.Monad
 import System.IO
-
-res allHash tasker = filter (\x ->(hash x) `elem` allHash)  tasker
+import Data.List.Split
+import Data.List
 
 hash :: String -> String
 hash = show.(MD5.md5).(BLU.fromString)
 
-workerLoop :: MVar [String] -> MVar [String] ->[String] -> MVar Int -> Int -> IO ()
-workerLoop taskQueue resultQueue hashList countOfTask  0 = do
+res :: [String] ->[String] -> [String]
+res allHash tasker = filter (\x ->(hash x) `elem` allHash)  tasker
+
+workerLoop :: [String] ->[String] -> MVar Int -> Int -> IO ()
+workerLoop taskQueue hashList countOfTask  0 = do
   putStrLn "!!!one core completed work!!!"
   return ()
-workerLoop taskQueue resultQueue hashList countOfTask cou =  do
-  maybeTask <- (modifyMVar taskQueue  (\q -> return $ case q of
-                                                                                                    [] -> ([], Nothing)
-                                                                                                    xs -> (drop 1 xs, Just (take 1 xs)))) --колебания: 1 - 16 сек 100+ 10 сек
-                                                                                                    --также, чем больше, тем больше съест оперативы
-                                                                                                    --при слишком малом числе тратим время на рекурсивный вызов и проверки кейсов
-                                                                                                    --при большом числе больше охват, но и дольше время выполнения (250000 - 400МБ оперативы)
+workerLoop taskQueue hashList countOfTask cou =  do
+  maybeTask <- return (take 10000 taskQueue)
   case maybeTask of
-    Nothing -> do
-      let str = "Can't found something... =("
-      putStrLn str
-      return ()
-    Just task -> do
+    [] -> do
+      putStrLn "Can't found something... =("
+      workerLoop taskQueue hashList countOfTask 0
+    task -> do
       let resus = res hashList task
       case resus of
         [] -> do
           trya <- readMVar countOfTask
-          workerLoop taskQueue resultQueue hashList countOfTask trya
+          workerLoop (drop 10000 taskQueue) hashList countOfTask trya
         smth -> do
           putStrLn ("Your password is: "++(show smth))
           modifyMVar_ countOfTask (\q -> return $ q-length(smth))
           trya <- readMVar countOfTask
-          workerLoop taskQueue resultQueue hashList countOfTask (trya)
+          workerLoop (drop 10000 taskQueue) hashList countOfTask trya
 
 main :: IO ()
 main = do
       let hashh context = lines context
           alphabet = ['0'..'9'] ++ ['a'..'z'] ++ ['A'..'Z']
           allPasswords = mapM (const alphabet) [()] ++ mapM (const alphabet) [(),()]  ++ mapM (const alphabet) [(),(),()]  ++ mapM (const alphabet) [(),(),(),()]  ++ mapM (const alphabet) [(),(),(),(),()]
+          splitList :: Int -> [String] -> [[String]]
+          splitList n xs = transpose $ chunksOf n xs
+          powar :: [[String]] -> MVar Int -> Int ->String -> Int -> Int -> IO ()
+          powar (x:xs) countOfTaskMVAR countOfTask contentFromFile num jader | num == jader - 1 = do {forkIO (workerLoop x (hashh contentFromFile)  countOfTaskMVAR countOfTask); return()}
+                                                                                                                              | otherwise = do
+                                                                                                                                forkIO (workerLoop x (hashh contentFromFile)  countOfTaskMVAR countOfTask)
+                                                                                                                                powar xs countOfTaskMVAR countOfTask contentFromFile (num+1) jader
 
       hFile <- openFile "input.txt" ReadMode
       contentFromFile <- hGetContents hFile
       workerNumber <- getNumCapabilities
       putStrLn ("!WARNING! Please wait the end of " ++ (show workerNumber) ++ " processes !WARNING!")
-      taskQueue <- newMVar allPasswords
-      resultQueue <- newMVar []
       countOfTaskMVAR <- newMVar (length (hashh contentFromFile))
       countOfTask <- readMVar countOfTaskMVAR
-      workerNumber `replicateM_` forkIO (workerLoop taskQueue resultQueue (hashh contentFromFile)  countOfTaskMVAR countOfTask)
+      powar (splitList (workerNumber) allPasswords) countOfTaskMVAR countOfTask contentFromFile 0 workerNumber
       time <- getChar
       hClose hFile
       return()
